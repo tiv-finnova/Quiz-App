f1282b480fd6c1b3e64759c4be1bf4d001136d62
tiv-finnova <timo.vonarx@finnova.com>
2025-01-03 09:31:28 +0100

    Revert

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ExampleTest2Component } from './example-test-2/example-test-2.component';
 import { ExampleTestComponent } from './example-test/example-test.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'Login', pathMatch: 'full' },
   { path: 'Quiz/1', component: ExampleTestComponent },
-  { path: 'Quiz/2', component: ExampleTest2Component },
   { path: 'Login', component: LoginComponent },
   { path: 'Dashboard', component: DashboardComponent},
 ];
